a32ab28959eb615012c5a9742a9579281a9d6884
Staline Ngoma <ngomastaline@gmail.com>
2024-07-08 13:02:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function GoalForm(props) {
+  const [formData, setFormData] = useState({
+    goal: "",
+    by: ""
+  })
+
+  function changeHandler(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function submitHandler(e) {
+      e.preventDefault()
+      props.onAdd(formData)
+      setFormData({
+        goal: "",
+        by: ""
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>My Little Lemon Goal</h1>
+      <div>
+        <form onSubmit={submitHandler}>
+          <input 
+            type='text'
+            name='goal'
+            placeholder='Goal'
+            value={formData.goal}
+            onChange={changeHandler}
+          />
+          <input 
+            type='text'
+            name='by'
+            placeholder='By...'
+            value={formData.by}
+            onChange={changeHandler}
+          />
+          <button type='submit'>Sumit Goal</button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+function ListOfGoals(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>My goal is to {g.goal}, by {g.by}</span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+
+
+function App() {
+  const [allGoals, updateAllGoals] = useState([])
+
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal])
+  }
+
+  return (
+    <div>
+      <GoalForm onAdd={addGoal}/>
+      <ListOfGoals allGoals={allGoals} />
     </div>
   );
 }
